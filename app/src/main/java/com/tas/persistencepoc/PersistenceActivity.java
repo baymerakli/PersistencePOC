3088397cf46dc94a449d58e7151d7a68d1020110
baymerakli <ozgurtas@gmail.com>
2015-10-17 17:19:20 +0300

    savng file and sharedPreferences

--- app/src/main/java/com/tas/persistencepoc/PersistenceActivity.java
+++ app/src/main/java/com/tas/persistencepoc/PersistenceActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class PersistenceActivity extends AppCompatActivity {
 
@@ -39,6 +45,76 @@ public class PersistenceActivity extends AppCompatActivity {
 
         appRestartsView.setText(String.valueOf(howManyTimesBeenRun));
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        readingView.setText(getTextFile());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveTextFile(readingView.getText().toString());
+    }
+
+    private static final String DATA_FILE="my_file";
+
+//    reading and writing
+
+    public String getTextFile(){
+        FileInputStream fileInputStream = null;
+        String fileData = null;
+        try {
+            fileInputStream = openFileInput(DATA_FILE);
+            int size = fileInputStream.available();
+            byte [] buffer = new byte[size];
+            fileInputStream.read(buffer);
+            fileInputStream.close();
+            fileData = new String(buffer,"UTF-8");
+        } catch (FileNotFoundException e){
+            Log.e("FILE", "Could not find that file");
+            e.printStackTrace();;
+        } catch (IOException e){
+            Log.e("FILE", "Error");
+            e.printStackTrace();;
+        } finally {
+            try {
+                if(fileInputStream != null){
+                    fileInputStream.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+        return fileData;
+    }
+
+    public void saveTextFile(String content){
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = openFileOutput(DATA_FILE,Context.MODE_PRIVATE);
+            fileOutputStream.write(content.getBytes());
+        } catch (FileNotFoundException e){
+            Log.e("FILE", "Could not find that file");
+            e.printStackTrace();;
+        } catch (IOException e){
+            Log.e("FILE", "IO Error");
+            e.printStackTrace();;
+        } finally {
+            try {
+                if(fileOutputStream != null){
+                    fileOutputStream.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
